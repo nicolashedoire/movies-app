1505f7c7fca9dc02b19090063f1b4c68f7043fd9
nicolashedoire <nicolashedoire@hotmail.com>
2020-10-21 22:31:28 +0200

    manage profile

--- src/pages/auth/selectors.ts
+++ src/pages/auth/selectors.ts
@@ -1,9 +1,5 @@
 import { RootState } from "../../store/rootReducer";
 
-export const getAuth = (state: RootState) => {
-  return state.firebase.auth;
-};
-
-export const getProfile = (state: RootState) => {
-  return state.firebase.profile;
-};
+export const getAuth = (state: RootState) => state.firebase.auth;
+export const getUid = (state: RootState) => state.firebase.auth.uid;
+export const getProfile = (state: RootState) => state.firebase.profile;
--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,16 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Row, Col, Button, Container, Form } from "reactstrap";
 import { useSelector } from "react-redux";
-import { getAuth, getProfile } from "../../pages/auth/selectors";
+import { getProfile, getUid } from "../../pages/auth/selectors";
+import { firestore } from "../../firebase";
 
 export default function Profile() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
   const profile = useSelector(getProfile);
+  const uid = useSelector(getUid);
+
+  useEffect(() => {
+    if (profile.lastname && profile.firstname) {
+      setLastname(profile.lastname);
+      setFirstname(profile.firstname);
+    }
+  }, [profile]);
+
+  useEffect(() => {}, [firstname, lastname]);
+
+  const updateProfile = () => {
+    if(!uid){
+      return;
+    }
+    return firestore
+      .collection("users")
+      .doc(uid)
+      .set({
+        firstname,
+        lastname,
+        initials: `${firstname[0].toUpperCase()}${lastname[0].toUpperCase()}`,
+      });
+  };
 
   return (
     <Container fluid>
-      <h1 className="mt-4">Profil</h1>
-      {profile.lastname ? <p>Nom : {profile.lastname}</p> : null}
-      {profile.firstname ? <p>Prénom : {profile.firstname}</p> : null}
+      <Row>
+        <Col md={12}>
+          <h1 className="mt-4">Profil</h1>
+        </Col>
+        <Col md={12} className="mb-4 mt-4">
+          <Input
+            className="mb-4"
+            type="text"
+            value={lastname}
+            placeholder="Nom"
+            autoComplete="off"
+            onChange={(e) => setLastname(e.currentTarget.value)}
+          />
+          <Input
+            type="text"
+            value={firstname}
+            placeholder="Prénom"
+            autoComplete="off"
+            onChange={(e) => setFirstname(e.currentTarget.value)}
+          />
+        </Col>
+        <Col md={12}>
+          <Button
+            color="primary"
+            onClick={updateProfile}
+          >
+            Mettre à jour le profil
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
 }
